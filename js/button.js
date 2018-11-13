82ad95f05e96953571ff715cbac1f626af3c61f3
Bodiwho <44253549+Bodiwho@users.noreply.github.com>
2018-11-13 20:20:02 +0100

    good

--- js/button.js
+++ js/button.js
@@ -1,6 +1,6 @@
 var arr = ["tops/books.html",
 , 
-"tops/food.html",  "tops/people.html", ];
+"tops/food.html",  "tops/people.html"];
 
 function randSite(){
         location.href = "" + arr[Math.floor (Math.random()*arr.length)];
